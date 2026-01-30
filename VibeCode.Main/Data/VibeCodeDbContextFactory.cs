a175e009b2038ef7911deb28124957436a7115d8
Duy Ho Duc <duy123a@gmail.com>
2026-01-30 16:58:12 +0700

    feat: add database migrations for auth entities and main schema
    - Add AuthEntities migration for IdentityServer (auth schema)
    - Create Menu, Permission, RolePermission, UserPermission, MenuPermission tables
    - Add InitialSetup migration for Main app (main schema)
    - Add VibeCodeDbContextFactory for design-time DbContext creation

---
+++ VibeCode.Main/Data/VibeCodeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace VibeCode.Main.Data
+{
+    public class VibeCodeDbContextFactory : IDesignTimeDbContextFactory<VibeCodeDbContext>
+    {
+        public VibeCodeDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<VibeCodeDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new VibeCodeDbContext(optionsBuilder.Options, null!);
+        }
+    }
+}
